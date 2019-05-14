f503d89700d86e1d3ebccde3587366695e133805
James Clarke <jrtc27@jrtc27.com>
2019-05-14 15:16:04 +0100

    Support .chericap on CHERI RISC-V

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
@@ -12,6 +12,7 @@
 
 #include "RISCVELFStreamer.h"
 #include "MCTargetDesc/RISCVAsmBackend.h"
+#include "RISCVFixupKinds.h"
 #include "RISCVMCTargetDesc.h"
 #include "Utils/RISCVBaseInfo.h"
 #include "llvm/BinaryFormat/ELF.h"
@@ -76,3 +77,46 @@ void RISCVTargetELFStreamer::emitDirectiveOptionRelax() {}
 void RISCVTargetELFStreamer::emitDirectiveOptionNoRelax() {}
 void RISCVTargetELFStreamer::emitDirectiveOptionCapMode() {}
 void RISCVTargetELFStreamer::emitDirectiveOptionNoCapMode() {}
+
+void RISCVELFStreamer::EmitCheriIntcap(int64_t Value, unsigned CapSize, SMLoc) {
+  unsigned XLenInBytes = Is64Bit ? 8 : 4;
+  assert(CapSize == 2 * XLenInBytes);
+
+  if (Value == 0) {
+    EmitZeros(CapSize);
+  } else {
+    // TODO: we should probably move the CHERI capability encoding somewhere else.
+    // Maybe to BinaryFormat or Object?
+    // NB: Opposite order to MIPS due to endianness.
+    EmitIntValue(Value, XLenInBytes);
+    EmitIntValue(0, XLenInBytes);
+  }
+}
+
+void RISCVELFStreamer::EmitCheriCapabilityImpl(const MCSymbol *Symbol,
+                                              int64_t Offset, unsigned CapSize,
+                                              SMLoc Loc) {
+  visitUsedSymbol(*Symbol);
+  MCContext &Context = getContext();
+
+  const MCSymbolRefExpr *SRE =
+      MCSymbolRefExpr::create(Symbol, MCSymbolRefExpr::VK_None, Context);
+  const MCBinaryExpr *CapExpr =
+      MCBinaryExpr::createAdd(SRE,
+                              MCConstantExpr::create(Offset, Context),
+                              Context);
+
+  const unsigned ByteAlignment = CapSize;
+  insert(new MCAlignFragment(ByteAlignment, 0, 1, ByteAlignment));
+  // Update the maximum alignment on the current section if necessary.
+  MCSection *CurSec = getCurrentSectionOnly();
+  if (ByteAlignment > CurSec->getAlignment())
+    CurSec->setAlignment(ByteAlignment);
+
+  MCDataFragment *DF = new MCDataFragment();
+  MCFixup CapFixup =
+      MCFixup::create(0, CapExpr, MCFixupKind(RISCV::fixup_riscv_capability));
+  DF->getFixups().push_back(CapFixup);
+  DF->getContents().resize(DF->getContents().size() + CapSize, '\xca');
+  insert(DF);
+}
