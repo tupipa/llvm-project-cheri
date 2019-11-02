ce0770e493c6a0707374191c83dcfbc7703f6fdf
Lele Ma <lma17@ur.rochester.edu>
2019-11-02 11:02:19 -0400

    Error with RegisterStandardPasses
    /root/cheri/llvm-project/llvm/lib/Transforms/PrivilegeDataTrack/PrivilegeDataTrack.cpp:56:41: error: member access into incomplete type 'legacy::PassManagerBase'
           legacy::PassManagerBase &PM) { PM.add(new PrivilegeDataTrack()); });
                                            ^
    /root/cheri/llvm-project/llvm/include/llvm/Transforms/IPO/PassManagerBuilder.h:31:7: note: forward declaration of 'llvm::legacy::PassManagerBase'
    class PassManagerBase;
          ^
    /root/cheri/llvm-project/llvm/lib/Transforms/PrivilegeDataTrack/PrivilegeDataTrack.cpp:96:41: error: member access into incomplete type 'legacy::PassManagerBase'
           legacy::PassManagerBase &PM) { PM.add(new PrivilegeDataTrack2()); });
                                            ^
    /root/cheri/llvm-project/llvm/include/llvm/Transforms/IPO/PassManagerBuilder.h:31:7: note: forward declaration of 'llvm::legacy::PassManagerBase'
    class PassManagerBase;
          ^
    2 errors generated.

--- llvm/lib/Transforms/PrivilegeDataTrack/PrivilegeDataTrack.cpp
+++ llvm/lib/Transforms/PrivilegeDataTrack/PrivilegeDataTrack.cpp
@@ -15,6 +15,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 using namespace llvm;
 
 #define DEBUG_TYPE "privilegeDataTrack"
